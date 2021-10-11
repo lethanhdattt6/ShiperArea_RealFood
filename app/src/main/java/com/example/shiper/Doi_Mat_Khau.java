5fa29b131191d886dccb2d7d6fcb2c68bf65d7a2
lethanhdattt6 <57130099+lethanhdattt6@users.noreply.github.com>
2021-10-11 11:01:07 +0700

    uodate

---
+++ app/src/main/java/com/example/shiper/Doi_Mat_Khau.java
@@ -0,0 +1,14 @@
+package com.example.shiper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Doi_Mat_Khau extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doi_mat_khau);
+    }
+}
